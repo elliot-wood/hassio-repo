b7557430d555658ceeed71d2c2dd8a4a4fd8b15e
Elliot Wood <elliot-wood@users.noreply.github.com>
2020-08-16 06:10:36 +1000

    Add alpine-based source package

---
+++ shairport-sync-src/README.md
@@ -0,0 +1,12 @@
+# Shairport Sync
+
+Stream music to your Home Assistant device
+via Apple AirPlay. Supports a variety of
+customisation options to allow configuring
+Shairport to taste.
+
+This is the Alpine based source version. It is more recent
+compared to the Ubuntu version, but requires building
+from source, which may take a while on low powered
+devices, and could cause considerable wear to your
+SD card if you are using a Raspberry Pi or similar.
---
+++ shairport-sync-src/rootfs/etc/cont-init.d/preflight.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Sets up the configuration files for Shairport-Sync
+# ==============================================================================
+declare airplay_name
+declare avahi_hostname
+declare avahi_domain
+declare ap_password
+declare ap_interpolation
+declare ap_port
+declare ap_drift
+declare ap_resync
+declare ap_ignore_volume
+declare ap_statistics
+declare ap_playback_mode
+declare log_level
+
+
+if ! bashio::config.has_value 'avahi_hostname'; then
+    bashio::log.fatal
+    bashio::log.fatal "Add-on configuration is incomplete!"
+    bashio::log.fatal "Set the hostname to the mDNS name of the server."
+    bashio::log.fatal "Please set the 'avahi_hostname' add-on option."
+    bashio::log.fatal
+    bashio::exit.nok
+fi
+
+
+# /etc/shairport-sync.conf
+# TODO: make this not incredibly cursed
+{
+	echo "general ="
+	echo "{"
+} > /etc/shairport-sync.conf
+
+if bashio::config.has_value 'airplay_name'; then
+	airplay_name=$(bashio::config 'airplay_name')
+	{
+		echo "name = \"${airplay_name}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+if bashio::config.has_value 'ap_password'; then
+	ap_password=$(bashio::config 'ap_password')
+	{
+		echo "password = \"${ap_password}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+if bashio::config.has_value 'ap_interpolation'; then
+	ap_interpolation=$(bashio::config 'ap_interpolation')
+	{
+		echo "interpolation = \"${ap_interpolation}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+if bashio::config.has_value 'ap_port'; then
+	ap_port=$(bashio::config 'ap_port')
+	{
+		echo "port = \"${ap_port}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+if bashio::config.has_value 'ap_drift'; then
+	ap_drift=$(bashio::config 'ap_drift')
+	{
+		echo "drift = \"${ap_drift}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+if bashio::config.has_value 'ap_resync'; then
+	ap_resync=$(bashio::config 'ap_resync')
+	{
+		echo "resync_threshold = \"${ap_resync}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+if bashio::config.has_value 'ap_ignore_volume'; then
+	ap_ignore_volume=$(bashio::config 'ap_ignore_volume')
+	{
+		echo "ignore_volume_control = \"${ap_ignore_volume}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+if bashio::config.has_value 'ap_statistics'; then
+	ap_statistics=$(bashio::config 'ap_statistics')
+	{
+		echo "statistics = \"${ap_statistics}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+if bashio::config.has_value 'ap_playback_mode'; then
+	ap_playback_mode=$(bashio::config 'airplay_name')
+	{
+		echo "playback_mode = \"${ap_playback_mode}\""
+	} >> /etc/shairport-sync.conf
+fi
+
+{
+    echo "};"
+} >> /etc/shairport-sync.conf
+
+
+# /etc/avahi/avahi-daemon.conf
+avahi_hostname=$(bashio::config 'avahi_hostname')
+avahi_domain=$(bashio::config 'avahi_domain')
+{
+	echo "[server]"
+	echo "host-name=${avahi_hostname}"
+	echo "domain-name=${avahi_domain}"
+	echo "allow-interfaces=eth0"
+} > /etc/avahi/avahi-daemon.conf

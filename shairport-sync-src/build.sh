a22fbc6d4d4deaf8dfc479f8ebd87470a7beb27a
Elliot Wood <elliot-wood@users.noreply.github.com>
2020-08-16 17:06:58 +1000

    Add runtime deps to build script
    also fixes cleanup phase of build

--- shairport-sync-src/build.sh
+++ shairport-sync-src/build.sh
@@ -1,8 +1,14 @@
-apk add avahi mbedtls
+#!/bin/sh
+
+# install runtime deps
+apk add dbus avahi mbedtls libconfig alsa-lib popt libsndfile soxr
+
+# install build deps
 apk add build-base git xmltoman autoconf automake libtool libdaemon-dev popt-dev avahi-dev alsa-lib-dev soxr-dev mbedtls-dev libsndfile-dev libconfig-dev
 
 cd ~
 
+# compile and install Apple ALAC library
 git clone 'https://github.com/mikebrady/alac.git'
 cd alac
 autoreconf -i -f
@@ -12,6 +18,7 @@ make install
 ldconfig
 cd ..
 
+# compile and install shairport-sync
 git clone 'https://github.com/mikebrady/shairport-sync.git'
 cd shairport-sync
 autoreconf -i -f
@@ -20,8 +27,9 @@ make
 make install
 cd ..
 
-rm -rf alac shairport-sync
-
+# remove build dependencies
 apk del --purge build-base git xmltoman autoconf automake libtool libdaemon-dev popt-dev avahi-dev alsa-lib-dev soxr-dev mbedtls-dev libsndfile-dev libconfig-dev
 
-rm -rf /var/cache/*
+# clean up the image
+rm -rf alac shairport-sync
+rm -rf /var/cache/{log,apk}/*
